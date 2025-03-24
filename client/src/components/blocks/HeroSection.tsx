e4ed91b2204e1bc0e0bb479fb7234e4ea1d548e5
Rasheed124 <rashdevatrealglobe@gmail.com>
2025-03-24 13:34:14 +0100

    adding section layouts

---
+++ client/src/components/blocks/HeroSection.tsx
@@ -0,0 +1,21 @@
+import { HeroSectionProps } from "@/type";
+import Link from "next/link";
+// import { StrapiImage } from"../StrapiImage";
+// import type { HeroSectionProps } from "@/types";
+
+export function HeroSection({
+  theme,
+  heading,
+  cta,
+  image,
+  logo,
+  author,
+  publishedAt,
+  darken = false,
+}: Readonly<HeroSectionProps>) {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
